57386ec082b9ecdb7abe1dd1c69e9eca80de089d
Isaac Hung <manhei5001@gmail.com>
2022-11-02 08:49:27 +0800

    add zoom workspace

--- config.h
+++ config.h
@@ -50,6 +50,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   isterminal	noswallow	monitor */
 	{ "st",       NULL,       NULL,       0,            0,           1,             0,        	-1 },
 	{ "discord",  NULL,       NULL,       1 << 8,       0,		 0,		0,		-1 },
+	{ "zoom",     NULL,       NULL,       1 << 7,       0,		 0,		0,		-1 },
 	{ NULL,       NULL,       "Event Tester",  0,       0,           0,             1,        	-1 }, /* xev */
 };
 
